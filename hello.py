c196f11695682accc3d223dc7c477c5d214c65c7
Faris Zulkiffli <22FTT1517@student.pb.edu.bn>
2026-01-21 10:01:54 +0800

    first week just normal trying python with some easy codes seeing how it works

---
+++ hello.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+msg = "Roll a dice!"
+print(msg)
+
+msg2 = msg.capitalize()
+print(msg2)  
+
+msg3 = msg.split()
+print(msg3)
+
+print(np.random.randint(1,9))  # Roll a dice with values from 1 to 8
